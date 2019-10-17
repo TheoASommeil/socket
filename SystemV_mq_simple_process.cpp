33d285f9988761aede272df7c798100443fd9a9b
TheoASommeil <TheoASommeil@users.noreply.github.com>
2019-10-17 23:32:49 +0800

    完善代码

--- SystemV_mq_simple_process.cpp
+++ SystemV_mq_simple_process.cpp
@@ -30,19 +30,19 @@ int main(int argc, char** argv)
 	--argc; ++argv;
 
 	if (0 == strncmp("-c", argv[1])) {
-		msgcreate(--argc, ++argv);
+		msgcreate(argc, argv);
 		_exit(0);
 	} else if (0 == strncmp("-s", argv[1])) {
-		msgsnd(--argc, ++argv);
+		msgsend(argc, argv);
 		_exit(0);
 	} else if (0 == strncmp("-r", argv[1])) {
-		msgrcv(--argc, ++argv);
+		msgrecv(argc, argv);
 		_exit(0);
 	} else if (0 == strncmp("-r", argv[1])) {
-		msgrmid(--argc, ++argv);
+		msgrmid(argc, argv);
 		_exit(0);
 	} else {
-		printf("cant recogenize %s, usage --help for more info.\n", argv[1]);
+		printf("can't recogenize %s, usage --help for more info.\n", argv[1]);
 		_exit(0);
 	}
 }
@@ -54,20 +54,85 @@ void show_help()
 
 void msgcreate(int argc, char** argv)
 {
+	int oflag = SVMSG_MODE | IPC_CREAT;
+	int i = 0;
 
+	while ( (i = getopt(argc, argv, "e")) != -1) {
+		switch (i) {
+			case 'e' :
+				oflag |= IPC_EXCL;
+				break;
+		}
+	}
+
+	if (optind != argc - 1) {
+		printf("usage: sysV_simp -c [ -e ] <pathname>\n");
+		_exit(0);
+	}
+
+	int mqid = msgget(ftok(argv[optind]), oflag);
+	_exit(0);
 }
 
-void msgsnd(int argc, char** argv)
+void msgsend(int argc, char** argv)
 {
+	if (argc != 3) {
+		printf("usage: sysV_simp -s <pathname> <#bytes> <type>\n");
+		_exit(0);
+	}
 
+	int len = atoi(argv[1]);
+	long type = atol(argv[2]);
+	int mqid = msgget(ftok(argv[0], 0), MSG_W);
+
+	struct msgbuf* ptr = calloc(sizeof(long) + len, sizeof(char));
+	ptr->mtype = type;
+	msgsnd(mqid, ptr, len, 0);
+	_exit(0);
 }
 
-void msgrcv(int argc, char** argv)
-{
+#define MAXMSG (8192 + sizeof(long))
 
+void msgrecv(int argc, char** argv)
+{
+	int i = 0;
+	int flag = 0;
+	long type = 0;
+
+	while ( (i = getopt(argc, argv, "nt:")) != -1) {
+		switch (i) {
+			case 'n':
+			flag |= IPC_NOWAIT;
+			break;
+			case 't':
+			type = atol(optarg);
+			break;
+		}
+	}
+
+	if (optind != argc -1) {
+		printf("usage: sysV_simp -r  [ -n ] [ -t type ] <pathname>\n");
+		_exit(0);
+	}
+
+	int mqid = msgget(ftok(argv[optind], 0), MSG_R);
+
+	struct msgbuf* buff;
+	buff = calloc(MAXMSG);
+
+	ssize_t n = nsgrcv(mqid, buff, MAXMSG, type, flag);
+	printf("read %d bytes, type = %ld\n", n, buff->mtype);
+	_exit(0);
 }
 
 void msgrmid(int argc, char** argv)
 {
+	if (argc != 1) {
+		printf("usage: sysV_simp -r <pathname>");
+		_exit(0);
+	}
 
+	int mqid = msgget(ftok(argv[0], 0), 0);
+	msgctl(mqid, IPC_RMID, NULL);
+	_exit(0);
 }
